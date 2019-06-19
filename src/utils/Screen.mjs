32a18e41867c2c480d480575c95ea603038a137c
Weazel <kagayushi@hotmail.de>
2019-06-19 22:01:54 +0200

    Updated for latest beta

--- src/utils/Screen.mjs
+++ src/utils/Screen.mjs
@@ -2,9 +2,10 @@
 import * as alt from 'alt';
 import * as game from 'natives';
 
-const [_, x, y] = game.getActiveScreenResolution();
+//const [_, x, y] = game.getActiveScreenResolution();
+const screen = game.getActiveScreenResolution();
 
 export const Screen = {
-    width: x,
-    height: y
+    width: screen[1],
+    height: screen[2]
 };
